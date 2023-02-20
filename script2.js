1434d2a21719f3092c91476ce7745a4908ef19dc
Radhikasuresh <124970046+Radhikasuresh@users.noreply.github.com>
2023-02-20 19:28:20 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,51 @@
+let myResume={
+    "basics": {
+      "name": "Radhika Suresh",
+      "email": "radhikasures@gamil.com",
+      "phone": 8610119947,
+      "degree": "B.E",
+      "location": {
+        "address": "vadapalani,chennai",
+        "postalCode": 600026,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    },
+    "work": [
+      {
+        "company": "Miramed Ajuba",
+        "position": "AR analyst",
+        "startDate": "2021-02-16",
+        "endDate": "2022-12-16",
+       
+      },
+    ],
+    "education": [
+      {
+        "institution": "saveetha engineering college",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+       
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+  
+  }
+  console.log(myResume);
+
+
